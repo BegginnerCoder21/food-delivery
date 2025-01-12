da6a6e459d111dd9194b3c771e32860540b8e0fd
Teguera Aboubacar <aboubacarteguera21@gmail.com>
2025-01-12 20:19:32 +0000

    relancement du projet

---
+++ src/main/java/com/api/food_delivery_api/entity/User.java
@@ -0,0 +1,17 @@
+package com.api.food_delivery_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
