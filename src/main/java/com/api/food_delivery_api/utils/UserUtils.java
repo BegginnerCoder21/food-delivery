a575c41e70716633b80b34270d45aa045cd83896
Teguera Aboubacar <aboubacarteguera21@gmail.com>
2025-01-19 15:33:38 +0000

    fin du projet

--- src/main/java/com/api/food_delivery_api/utils/UserUtils.java
+++ src/main/java/com/api/food_delivery_api/utils/UserUtils.java
@@ -1,5 +1,6 @@
 package com.api.food_delivery_api.utils;
 
+import com.api.food_delivery_api.exeption.InternalServerErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
@@ -17,7 +18,7 @@ public class UserUtils {
             return;
         }
 
-        throw new IllegalArgumentException("Le nom d'utilisateur est vide !");
+        throw new InternalServerErrorException("Le nom d'utilisateur est vide !");
     }
 
     public static void phoneNumberNotEmpty(String phoneNumber)
@@ -27,7 +28,7 @@ public class UserUtils {
             return;
         }
 
-        throw new IllegalArgumentException("Le numero fournit est vide !");
+        throw new InternalServerErrorException("Le numero fournit est vide !");
     }
 
     public static Date convertStringToDate(String dateOfBirth)
@@ -35,11 +36,12 @@ public class UserUtils {
         DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
         try {
+
             return dateFormat.parse(dateOfBirth);
+
         } catch (Exception e) {
             log.info("Erreur de conversion de date pour l'anniversaire");
-            
+            throw new InternalServerErrorException("Erreur de conversion de date pour l'anniversaire");
         }
-        return new Date();
     }
 }
